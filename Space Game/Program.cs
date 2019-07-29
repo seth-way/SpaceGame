06f816e078ed61805bf1b20f3156a7138ee33944
SethanielWay <52747612+SethanielWay@users.noreply.github.com>
2019-07-29 12:35:37 -0400

    initial project

---
+++ Space Game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Space_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
