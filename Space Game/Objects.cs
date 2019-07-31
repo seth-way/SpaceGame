0d8ebb2ba37af9039afbcb1cc35d76aa29dba2df
Justin A Hughes <justin.a.hughes24@outlook.com>
2019-07-31 15:54:25 -0400

    Updated Products with new goods and descriptions, added Storyline.cs with first story log and TextOutput function.

--- Space Game/Objects.cs
+++ Space Game/Objects.cs
@@ -12,10 +12,12 @@ namespace SpaceGame
         { xLoc = 2.0, yLoc = 3.75, inhabitants = "Proxima Centaurians", dangerRating = 2.0, name = "Proxima Centauri B" };
         public static readonly Planet Gazorpazorp = new Planet ()
         { xLoc = 14.0, yLoc = -6.0, inhabitants = "Gazorpazorp", dangerRating = 6.0 , name = "Gazorpazorp" };
-        public static readonly Planet Planet4 = new Planet ()
+        public static readonly Planet ScreamingSun = new Planet ()
         { xLoc = -3.0, yLoc = -5.5, inhabitants = "Tired Earthlings", dangerRating = 1.5, name = "Screaming Sun Earth" };
         public static readonly Planet C35 = new Planet ()
         { xLoc = -20.0, yLoc = 1.5, inhabitants = "Galactic Federation", dangerRating = 5.0, name = "35-C" };
+        public static readonly Planet GromflomPrime = new Planet ()
+        { xLoc = 0, yLoc = 0, inhabitants = "Genetically Engineered Gromflomites", dangerRating = 7.0, name = "Gromflom Prime"};
 
         //Possible planet name ideas: 
         //Rick and Morty: Gazopazop, On a Cob Planet, Cronenberg World, Gromflom Prime, Alphabetrium, Pluto, Screaming Sun Earth
@@ -44,33 +46,6 @@ namespace SpaceGame
 
     public class Products
     {
-        public Good MegaTreeSeeds = new Good ()
-        {
-            name = "Mega Tree Seeds",
-            price = 0.00,
-            size = 1,
-            originPlanet = Universe.C35.name,
-            description = "A highly controlled seed that was smuggled out of Planet 35-C. Causes the" +
-            "consumer to have temporary super intelligence. Don't ask how they were smuggled out."
-        };
-
-        public Good CentaurianFur = new Good ()
-        {
-            name = "Proxima Centaurian Fur",
-            price = 0.00,
-            size = 1,
-            originPlanet = Universe.ProximaCentauriB.name,
-        };
-
-        public Good ServiceRobot = new Good ()
-        {
-            name = "Gazorpian Service Robot",
-            price = 0.00,
-            size = 1,
-            originPlanet = Universe.Gazorpazorp.name,
-            description = ""
-        };
-
         public Good CannedAir = new Good ()
         {
             name = "Canned Earth Air",
@@ -79,6 +54,47 @@ namespace SpaceGame
             originPlanet = Universe.Earth.name,
             description = "A can of air from Earth. While it seems like an ordinary item to you, other" +
             "species that live in atmospheres different to Earth use this as a recreational drug."
+        };
+
+        public Good CentaurianFur = new Good ()
+        {
+            name = "Proxima Centaurian Fur",
+            price = 0.00,
+            size = 1,
+            originPlanet = Universe.ProximaCentauriB.name,
+            description = "The Centaurs of Proxima are reknowned for their soft yet strong fur. Although none of" +
+            "them actually have any visible fur. You try not to think about it."
+        };
+
+        public Good ServiceRobot = new Good ()
+        {
+            name = "Gazorpian Service Robot",
+            price = 0.00,
+            size = 1,
+            originPlanet = Universe.Gazorpazorp.name,
+            description = "A robot from the planet Gazorpazorp. You don't understand it's purpose," +
+            "but other species seem to be really eager to buy it."
+        };
+
+        public Good RealFakeDoors = new Good ()
+        {
+            name = "Real Fake Doors",
+            price = 500.00,
+            size = 1,
+            originPlanet = Universe.GromflomPrime.name,
+            description = "When you asked the salesman how he got to Gromflom he just blankly stared at the ground" +
+            "muttering something, you were barely able to hear the last bit of his explanation; 'The door was real" +
+            "but it wasn't real fake...' The doors are worthless. Maybe someone will use them for firewood."
+        };
+
+        public Good MegaTreeSeeds = new Good ()
+        {
+            name = "Mega Tree Seeds",
+            price = 0.00,
+            size = 1,
+            originPlanet = Universe.C35.name,
+            description = "A highly controlled seed that was smuggled out of Planet 35-C. Causes the" +
+            "consumer to have temporary super intelligence. Don't ask how they were smuggled out."
         };
     }
 }
